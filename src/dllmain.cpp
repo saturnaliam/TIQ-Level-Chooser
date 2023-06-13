91e81a4e1d79be8a9c993c0a743d29dab0657570
saturnaliam <llcsaturnaliam@gmail.com>
2023-06-13 12:05:58 -0500

    fixed it yippee

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,23 +1,24 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #include <iostream>
-#define nop 0x90
+#define NOP 0x90
 
-unsigned char* hook_location = (unsigned char*)0x001953B2;
-int goto_level = 0x9;
+unsigned char* hook_location = (unsigned char*)0x005253B2;
 
-__declspec(naked) void move_value() {
-    __asm {
-        mov dword ptr ss:[ebp + 8], 3
+unsigned char MOVE_OPCODES[] = {
+    0xC7, 0x45, 0x08, 0x09, 0x00, 0x00, 0x00
+};
+
+void move_value() {
+    for (int i = 0; i < 7; i++) {
+            *(hook_location + i) = MOVE_OPCODES[i];
     }
 }
 
 void clear_instructions() {
     for (int i = 0; i < 10; i++) {
-        *(hook_location + i) = nop;
+        *(hook_location + i) = NOP;
     }
-
-    return;
 }
 
 DWORD WINAPI attached_main(LPVOID lpParameter) {
@@ -31,7 +32,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         VirtualProtect((void*)hook_location, 10, PAGE_EXECUTE_READWRITE, &old_protect);
         clear_instructions();
-        *(DWORD*)hook_location = (DWORD)&move_value;
+        move_value();
     }
 
     return TRUE;
