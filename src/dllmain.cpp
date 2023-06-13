c57d6ff5897a2e51c6b7fa9573e42e7436d02226
saturnaliam <llcsaturnaliam@gmail.com>
2023-06-13 01:53:53 -0500

    added code that doesnt work

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,37 +1,37 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #include <iostream>
+#define nop 0x90
 
-__declspec(naked) void clear_instructions() {
+unsigned char* hook_location = (unsigned char*)0x001953B2;
+int goto_level = 0x9;
+
+__declspec(naked) void move_value() {
     __asm {
-        nop
-        nop
-        nop
-        nop
-        nop
-        nop
-        nop
-        nop
-        nop
-        nop
+        mov dword ptr ss:[ebp + 8], 3
     }
 }
 
-DWORD WINAPI attached_main(LPVOID lpParameter) {
+void clear_instructions() {
+    for (int i = 0; i < 10; i++) {
+        *(hook_location + i) = nop;
+    }
 
+    return;
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
+DWORD WINAPI attached_main(LPVOID lpParameter) {
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     DWORD old_protect;
-    unsigned char* hook_location = (unsigned char*)0x001953B2;
+
 
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         VirtualProtect((void*)hook_location, 10, PAGE_EXECUTE_READWRITE, &old_protect);
-        *(DWORD*)(hook_location) = (DWORD)&clear_instructions;
+        clear_instructions();
+        *(DWORD*)hook_location = (DWORD)&move_value;
     }
 
     return TRUE;
